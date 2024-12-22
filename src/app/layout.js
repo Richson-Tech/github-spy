30c24180b2c78f8a08ec87e911bd949636d5013d
Richson-Tech <folorunshoahmedolajide@gmail.com>
2024-12-23 00:01:29 +0100

    added mods

--- src/app/layout.js
+++ src/app/layout.js
@@ -22,7 +22,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} bg-[#0a0a0a] antialiased`}
       >
         <ToastContainer />
         {children}
