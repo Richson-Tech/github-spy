2653574e3d0faba7d6ae7ee9ae3276bf3edcea5c
Richson-Tech <folorunshoahmedolajide@gmail.com>
2024-12-23 00:01:14 +0100

    added mods

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -10,20 +10,20 @@ export default function Home() {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
-      <h1 className="text-3xl font-bold mb-6">GitHubSpy</h1>
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-3xl font-bold mb-6 text-white">GitHubSpy</h1>
       <div className="flex gap-4">
         <button
           onClick={() => handleTabSelection("developer")}
           className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
         >
-          Developer Tab
+         Spy as a Developer
         </button>
         <button
           onClick={() => handleTabSelection("client")}
           className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600"
         >
-          Client Tab
+       Spy as a Client 
         </button>
       </div>
     </div>
