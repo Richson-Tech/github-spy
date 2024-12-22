f5b8f766abca1490ed2ef53a4d0d8424ff6b55dd
Richson-Tech <folorunshoahmedolajide@gmail.com>
2024-12-23 00:01:49 +0100

    added mods

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,21 @@
-import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Home</h1>
-   </div>
+    <div className="flex flex-col items-center justify-center">
+      <h1 className="text-center text-3xl mt-20 lg:text-6xl lg:mt-56">
+        Before the Code Speaks, Let{" "}
+        <span className="text-[#696c6e]">GitHubSpy</span> Tell the Story.
+      </h1>
+      <p className="mt-4 text-xl lg:text-3xl">
+        for Software Developers & Clients.
+      </p>
+      <Link href="/login">
+
+      <div className="p-2 border-2 border-white rounded-xl mt-4 cursor-pointer">
+        Get started 👀{" "}
+      </div>
+      </Link>
+    </div>
   );
 }
